2fa631f170a71701266e913a2ac48de36848e903
Ben <Lai>
2024-04-26 21:25:11 +1000

    Localised Search

--- scripts/mods/Archivum Messelina/Archivum Messelina.lua
+++ scripts/mods/Archivum Messelina/Archivum Messelina.lua
@@ -106,7 +106,7 @@ local append_to_view_defs = function(defs)
   
 	defs.widget_definitions.search_label = UIWidget.create_definition({
 		{
-			value = "Search: ",
+			value = mod:localize("Search: "),
 			value_id = "text",
 			pass_type = "text",
 			style = table.clone(UIFontSettings.body),
